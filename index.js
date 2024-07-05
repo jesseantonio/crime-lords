9848d1d3f97615be503bf6bf8c2f2df35cd44470
Jessé Antônio <jesseantonio1321@gmail.com>
2024-07-04 23:55:09 -0300

    improve movements

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const movable = document.getElementById('movable');
     const movable2 = document.getElementById('movable2');
     const container = document.getElementById('container');
+    let side;
 
-    let topPosition = 225; // Posição inicial
-    let leftPosition = 225; // Posição inicial
+    let topPosition = 225;
+    let leftPosition = 225;
 
     const step = 5;
     let moveInterval;
-    function startWalking() {
+    function startWalking(movable) {
         movable.style.backgroundImage = "url('./Gangsters_1/Walk.png')";
         movable.classList.add("walk");
     }
@@ -31,6 +32,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
             case 'left':
                 if (leftPosition > 0) {
                     transformSide(movable, -1);
+                    side = "LEFT";
                     leftPosition -= step;
                 }
                 break;
@@ -42,6 +44,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
             case 'right':
                 if (leftPosition < (container.clientWidth - movable.clientWidth)) {
                     transformSide(movable, 1);
+                    side = "RIGHT";
                     leftPosition += step;
                 }
                 break;
@@ -58,19 +61,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
     window.addEventListener('keydown', (event) => {
         switch (event.keyCode) {
             case 37: // Arrow Left
-                startWalking();
+                startWalking(movable);
                 updatePosition('left');
                 break;
             case 38: // Arrow Up
-                startWalking();
+                startWalking(movable);
                 updatePosition('up');
                 break;
             case 39: // Arrow Right
-                startWalking();
+                startWalking(movable);
                 updatePosition('right');
                 break;
             case 40: // Arrow Down
-                startWalking();
+                startWalking(movable);
                 updatePosition('down');
                 break;
             case 32: // Space
@@ -78,37 +81,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 setTimeout(() => {
                     movable.style.backgroundImage = "url('./Gangsters_1/Walk.png')";
                 }, 2000);
-                movable2.style.left = `${225}px`;
-                movable2.style.top = `${225}px`;
-                const bullet = document.createElement('div');
-                bullet.classList.add('bullet');
-                bullet.style.top = `${topPosition + 90}px`;
-                bullet.style.left = `${leftPosition + 50}px`;
-                container.appendChild(bullet);
-                const bulletSpeed = 5;
-                const bulletInterval = setInterval(() => {
-                    const currentLeft = parseInt(bullet.style.left);
-                    if (currentLeft > container.clientWidth) {
-                        bullet.remove();
-                        clearInterval(bulletInterval);
-                    } else {
-                        bullet.style.left = `${currentLeft + bulletSpeed}px`;
-                        if (checkCollision(bullet, movable2)) {
-                            movable2.style.backgroundImage = "url('./Gangsters_1/Hurt.png')";
-                            movable2.classList.add("hurt");
-                            setTimeout(() => {
-                                movable2.classList.remove("hurt");
-                            }, 500)
-                            health--;
-                            bullet.remove();
-                            clearInterval(bulletInterval);
-                            if (health === 0) {
-                                movable2.style.backgroundImage = "url('./Gangsters_1/Dead.png')";
-                                movable2.classList.add('death');
-                            }
-                        }
-                    }
-                }, 10);
+                shoot(topPosition, leftPosition, container, movable2, side);
                 break;
         }
 
@@ -116,19 +89,68 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
 
     window.addEventListener('keyup', (event) => {
-        stopWalking();
+        stopWalking(movable);
     });
 });
+
+function shoot(topPosition, leftPosition, container, movable2, direction) {
+    const bullet = document.createElement('div');
+    bullet.classList.add('bullet');
+
+    if (direction === 'LEFT') {
+        bullet.style.top = `${topPosition + 90}px`;
+        bullet.style.left = `${leftPosition}px`;
+    } else {
+        bullet.style.top = `${topPosition + 90}px`;
+        bullet.style.left = `${leftPosition + 50}px`;
+    }
+
+    container.appendChild(bullet);
+
+    const bulletSpeed = 5;
+    const bulletInterval = setInterval(() => {
+        let currentLeft = parseInt(bullet.style.left);
+
+        if (direction === 'LEFT') {
+            if (currentLeft < 0) {
+                bullet.remove();
+                clearInterval(bulletInterval);
+            } else {
+                bullet.style.left = `${currentLeft - bulletSpeed}px`;
+            }
+        } else {
+            if (currentLeft > container.clientWidth) {
+                bullet.remove();
+                clearInterval(bulletInterval);
+            } else {
+                bullet.style.left = `${currentLeft + bulletSpeed}px`;
+            }
+        }
+
+        if (checkCollision(bullet, movable2)) {
+            movable2.style.backgroundImage = "url('./Gangsters_1/Hurt.png')";
+            movable2.classList.add("hurt");
+            setTimeout(() => {
+                movable2.classList.remove("hurt");
+            }, 500);
+            health--;
+            bullet.remove();
+            clearInterval(bulletInterval);
+            if (health <= 0) {
+                movable2.style.backgroundImage = "url('./Gangsters_1/Dead.png')";
+                movable2.classList.add('death');
+            }
+        }
+    }, 10);
+}
+
 
 function checkCollision(bullet, target) {
     const bulletRect = bullet.getBoundingClientRect();
     let targetRect = target.getBoundingClientRect();
 
-    if (targetRect.right - bulletRect.left < 78) {
-        return true;
-    }
-
-    return false;
+    return ((targetRect.right - bulletRect.left < 78 && targetRect.right - bulletRect.left > 53) 
+    && ((targetRect.top + 90) - bulletRect.top <= 30) && ((targetRect.top + 90) - bulletRect.top >= -35));
 }
 
 function transformSide(movable, value) {
